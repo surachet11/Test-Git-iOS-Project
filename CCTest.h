4efd974811ba6b4bc7a388c4bc9a41e3246a831f
Surachet Songsakaew <surachet11@hotmail.com>
2014-05-13 21:45:30 +0700

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by surachet on 5/13/2557 BE.
+//  Copyright (c) 2557 surachet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by surachet on 5/13/2557 BE.
+//  Copyright (c) 2557 surachet. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
